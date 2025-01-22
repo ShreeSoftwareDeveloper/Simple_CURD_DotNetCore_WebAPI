341a8921e1e5b2c41114d1e6948492bfb52d1a13
unknown <Shreenandansingh1234@gmail.com>
2025-01-22 19:28:36 +0530

    initial commit

---
+++ WebApplication1/Controllers/FirstController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : ControllerBase   
+    {
+        private readonly FirstContext _first;
+        public FirstController(FirstContext first)
+        {
+            _first = first;
+        }
+        [HttpGet]
+        public async Task<string> GetHello()
+        {
+            return "Hello Shree Your Api Working Fine. Thanks for create the api.";
+        }
+        [HttpGet("Get Employee")]
+        public async Task<ActionResult<IEnumerable<Employee>>> Get()
+        {
+            return await _first.Employees.ToListAsync();
+        }
+        [HttpGet("GetEmployee")]
+        public async Task<IEnumerable<Employee>> GetEmp()
+        {
+            return await _first.Employees.ToListAsync();
+        }
+        [HttpGet("GetEmployees")]
+        public async Task<List<Employee>> GetEmps()
+        {
+            return await _first.Employees.ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddEmployee(Employee employee)
+        {
+            var result= await _first.AddAsync(employee);
+                        await _first.SaveChangesAsync();
+            return result.Entity;
+        }
+        [HttpGet("GetById/{Id}")]
+        public async Task<ActionResult<Employee>> GetById(int Id)
+        {
+            return await _first.Employees.FindAsync(Id);
+        }
+        [HttpPut]
+        public async Task<string> UpdateEmployee(Employee employee)
+        {
+            var employees = await _first.Employees.FindAsync(employee.Id);
+            if(employees!=null)
+            {
+                employees.Name = employee.Name;
+                employees.Mobile = employee.Mobile;
+                await _first.SaveChangesAsync();
+            }
+            else
+            {
+                return "Id is not matched";
+            }
+            return "Data updated Succesfuuly";
+        }
+        [HttpDelete]
+        public async Task<string> DeleteEmployee(int id)
+        {
+            var result = await _first.Employees.FindAsync(id);
+           if(result!=null)
+            {
+               _first.Employees.Remove(result);
+                await _first.SaveChangesAsync();
+            }
+            else
+            {
+                return "Id not Matched";
+
+            }
+            return "Deleted Succefully";
+
+        }
+
+    }
+}
---
+++ WebApplication1/Models/FirstContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models;
+
+public partial class FirstContext : DbContext
+{
+    public FirstContext()
+    {
+    }
+
+    public FirstContext(DbContextOptions<FirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-9S35C9Q1\\SQLEXPRESS;Initial Catalog=First;Integrated Security=True;Trust Server Certificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Employee__3214EC072824C8D5");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.Mobile)
+                .HasMaxLength(12)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    internal async Task<ActionResult<Employee>> FindAsync(bool v)
+    {
+        throw new NotImplementedException();
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
